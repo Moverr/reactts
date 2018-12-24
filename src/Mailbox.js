16299df342933c1afc27bc4800021676f1a6bb90
Muyinda Rogers <moverr@gmail.com>
2018-12-24 22:24:34 +0300

    :mail box :

--- src/Mailbox.js
+++ src/Mailbox.js
@@ -1,18 +1,35 @@
 import React, { Component } from 'react';
 
 class Mailbox extends Component {
+
+     constructor(props) {
+         super(props);
+         this.getMst = this.getMst.bind(this);
+     }
+     
     render() {
+       this.validate();
         
     const unreadMessages = this.props.unreadMessages;
         return (
             <div>
                 <h1>Hello !</h1>
-                <h2>
-                    You have {unreadMessages.length} unread messages.
-                    </h2>
+                {this.getMst(unreadMessages)}
             </div>
         );
     }
+
+validate = () =>{
+    if(!this.props.unreadMessages){
+        return null;
+    }
+}   
+
+ getMst(unreadMessages) {
+    return <h2> You have {unreadMessages.length} unread messages. </h2>;
+}
+
+
 }
 
 export default Mailbox;
