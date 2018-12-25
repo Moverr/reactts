e656a4491f1a0ee84e42ed36bdb434f311f999f5
Muyinda Rogers <moverr@gmail.com>
2018-12-25 15:16:09 +0300

    :Dbuging :

--- src/Messages.js
+++ src/Messages.js
@@ -2,10 +2,11 @@ import React, { Component } from 'react'
 
  class Messages extends Component {
   render() {
-      if(!this.props.tmessages){
+      if(!this.props.tMessages){
         return null;
       }
       
+      //todo.. map function etc and move on as needed and also as required ::--
       const listItems = this.props.tMessages.map((message)=><li>{message}</li>);
 
     return (
