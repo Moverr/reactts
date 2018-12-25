3e157a4dde7f0d304b84adc0d18356d4db1e5107
Muyinda Rogers <moverr@gmail.com>
2018-12-25 16:33:45 +0300

    :Debugging :

--- src/Blog.js
+++ src/Blog.js
@@ -6,32 +6,8 @@ class Blog extends Component {
     render() {
 
         const posts = this.props.posts;
-        const sidebar = (
-            <ul>
-                    {
-                        posts.map((post)=>
-                        <li key={post.id}>
-                             {post.title} 
-                        </li>
-                    )
-                }   
-            </ul>             
-        );
-
-        const content =  ( 
-            <div>
-                {
-                    posts.map((post)=>
-                    <div key={post.id}>
-                    <h2>{post.title}</h2>
-                    <p>{post.content}</p>
-                    
-                        </div>
-                    )
-                }
-            </div>
-        );
-
+        const sidebar = this.getSideBar(posts);
+        const content =  this.getContent(posts);
 
 
         return (
@@ -40,6 +16,22 @@ class Blog extends Component {
                 {content}
             </div>
         )
+    }
+
+    getContent(posts) {
+        return (<div>            {posts.map((post) => <div key={post.id}>
+                <h2>{post.title}</h2>
+                <p>{post.content}</p>
+            </div>)}
+        </div>);
+    }
+
+    getSideBar(posts) {
+        return (<ul>
+            {posts.map((post) => <li key={post.id}>
+                {post.title}
+            </li>)}
+        </ul>);
     }
 }
 
